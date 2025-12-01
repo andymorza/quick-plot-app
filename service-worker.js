1f171f1209400bca3090823f104d227d6c0ce0c4
andymorza <andymorza12@gmail.com>
2025-12-01 22:35:31 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+﻿const CACHE_NAME = 'quick-plot-v1.8-cache';
+const urlsToCache = [
+  './',
+  'index.html',
+  'manifest.json'
+];
+
+
+// Install event: Caches all static assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
+// Fetch event: Serves content from the cache if available
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // No cache hit - fetch from network
+        return fetch(event.request);
+      })
+  );
+});
+
+
+// Activate event: Clears out old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
